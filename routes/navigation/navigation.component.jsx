65e1bee535a9b76859ee2b35452efdb8660804b6
TheXro <ravyyadav97@gmail.com>
2024-04-27 23:29:18 +0530

    added routes

---
+++ routes/navigation/navigation.component.jsx
@@ -0,0 +1,22 @@
+import { Outlet, Link } from "react-router-dom";
+import './navigation.styles.scss';
+
+const Navigation = () => {
+  return (
+    <>
+      <div className='navigation'>
+        <Link to='/' className='logo-container'>
+          <div><img  className="logo" src="../../assets/crown.svg" alt="" /></div>
+        </Link>
+        <div className='nav-links-container'>
+          <Link to='/' className="nav-link">Home</Link>
+          <Link to='/signIn' className="nav-link">Sign In</Link>
+          <Link to='/shop' className="nav-link">Shop</Link>
+        </div>
+      </div>
+      <Outlet />
+    </>
+  );
+};
+
+export default Navigation;
